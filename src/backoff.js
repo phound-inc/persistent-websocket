ced0340c97ea5e1299e73d361f89f6c5a20d9ff2
Doug Keen <doug@swoot.com>
2017-02-23 13:17:03 -0800

    Simplify back off class a bit

--- src/backoff.js
+++ src/backoff.js
@@ -2,31 +2,28 @@
  * Backoff based on "Decorrelated jitter" algorithm on https://www.awsarchitectureblog.com/2015/03/backoff.html
  */
 
-function randomBetween(min, max)  {
+function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
 export default class Backoff {
-  constructor(initialDelayMillis, maxDelayMillis, randomBetweenFunc=randomBetween) {
+  constructor(initialDelayMillis, maxDelayMillis, randomBetweenFunc = randomBetween) {
     this._initialDelay = initialDelayMillis;
     this._maxDelay = maxDelayMillis;
-    this._lastDelay = -1;
+    this._nextDelay = this._initialDelay;
     this._randomBetween = randomBetweenFunc;
   }
 
   reset() {
-    this._lastDelay = -1;
+    this._nextDelay = this._initialDelay;
   }
 
   nextDelay() {
-    if (this._lastDelay < 0) {
-      this._lastDelay = this._initialDelay;
-    } else {
-      this._lastDelay = Math.min(
-        this._maxDelay,
-        this._randomBetween(this._initialDelay, this._lastDelay*3),
-      );
-    }
-    return this._lastDelay;
+    const delay = this._nextDelay;
+    this._nextDelay = Math.min(
+      this._maxDelay,
+      this._randomBetween(this._initialDelay, delay * 3),
+    );
+    return delay;
   }
 }
