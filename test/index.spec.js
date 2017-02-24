578fbcf1ee9ebfc61241c7b4e9419ff79f0befee
Doug Keen <doug@swoot.com>
2017-02-24 15:47:37 -0800

    Knock out a couple more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -27,7 +27,7 @@ class FakeWebSocket {
       const self = this;
       setTimeout(() => {
         self[method](arg)
-      }, timeout);
+      }, parseInt(timeout));
     });
   }
 
@@ -35,7 +35,13 @@ class FakeWebSocket {
     this.readyState = val;
     if (val === READYSTATE.OPEN && this.onopen) {
       this.onopen({name: "open", target: this});
+    } else if (val === READYSTATE.CLOSED && this.onclose) {
+      this.onclose({name: "closed", target: this});
     }
+  }
+
+  close(code, reason) {
+    this.setReadyState(READYSTATE.CLOSED);
   }
 }
 
@@ -81,11 +87,61 @@ describe('PersistentWebsocket', function () {
     pws.open();
     expect(pingCalled).to.be.false;
     clock.tick(750);
-    fakeWebsocket.onmessage({whatever:"junk"});
+    fakeWebsocket.onmessage({whatever: "junk"});
     clock.tick(750);
     // Prior message should have reset the ping interval timer
     expect(pingCalled).to.be.false;
     clock.tick(300);
     expect(pingCalled).to.be.true;
+  });
+
+  it('skips pings when not configured', function () {
+    const pws = new PersistentWebsocket(`setReadyState|${READYSTATE.OPEN}|0`, {pingIntervalSeconds: 1});
+    pws.open();
+    clock.tick(60000); // Just make sure nothing explodes when the ping interval passes
+  });
+
+  it('automatically reconnects', function () {
+    const wsActions = [
+      `setReadyState|${READYSTATE.OPEN}|200`,
+      `close|0|1000`,
+    ];
+    const pws = new PersistentWebsocket(wsActions.join(','), {initialBackoffDelayMillis: 1});
+    pws.open();
+    clock.tick(900);
+    const firstWs = fakeWebsocket;
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.OPEN);
+    clock.tick(100);
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.CLOSED);
+    clock.tick(500);
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.OPEN);
+
+    // Make sure it's a new websocket
+    const secondWs = fakeWebsocket;
+    expect(firstWs).not.to.equal(secondWs);
+  });
+
+  it('closes and automatically reconnects on ping failure', function () {
+    const wsActions = [
+      `setReadyState|${READYSTATE.OPEN}|1`,
+    ];
+    let pingCalled = false;
+    const pws = new PersistentWebsocket(wsActions.join(','), {
+      pingSendFunction: (ws) => {
+        pingCalled = true;
+      },
+      pingIntervalSeconds: 1,
+      pingTimeoutMillis: 500,
+      initialBackoffDelayMillis: 1000,
+    });
+    pws.open();
+    clock.tick(100);
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.OPEN);
+    clock.tick(1000);
+    expect(pingCalled).to.be.true;
+    clock.tick(500);
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.CLOSED);
+    clock.tick(1100);
+    expect(fakeWebsocket.readyState).to.equal(READYSTATE.OPEN);
   });
 });
